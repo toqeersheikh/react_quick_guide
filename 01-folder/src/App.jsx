fdac587010543ee79d7981b60da08cd22569812e
Toqeer Sheikh <102228843+toqeersheikh@users.noreply.github.com>
2025-11-07 16:47:53 +0500

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1">Islam Vs JDM</h1>
+        <h2 id="child2">Welterweight Championship Match</h2>
+      </div>
+      <div id="uncle">
+        <h1>heading from sibling div</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
