7b1f9d6ddfe62bdf7eb3e7b0efd686808d665aa4
Toqeer Sheikh <102228843+toqeersheikh@users.noreply.github.com>
2025-11-10 08:36:15 +0500

    props done

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,9 +4,22 @@ import Navbar from "./components/navbar";
 const App = () => {
   return (
     <div className="parent">
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        user="Max"
+        img="https://images.unsplash.com/photo-1569292316763-0b667e9e960c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=900"
+      />
+      <Card
+        user="Nick"
+        img="https://images.unsplash.com/photo-1531750026848-8ada78f641c2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=900"
+      />
+      <Card
+        user="Dexter"
+        img="https://images.unsplash.com/photo-1704731297889-6d54dc06da74?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDc2fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=900"
+      />
+      <Card
+        user="Jane"
+        img="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=988"
+      />
     </div>
   );
 };
--- 03-props/src/components/card.jsx
+++ 03-props/src/components/card.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const card = () => {
+const card = (props) => {
+  // console.log(props);
   return (
     <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1761440567736-1bef26a5afff?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764"
-        alt=""
-      />
-      <h1>Amond Targerian]</h1>
-      <p>When you play game of thrones either you win or you die</p>
-      <button>View Profile</button>
+      <div>
+        <img src={props.img} alt="User Image" />
+      </div>
+      <div>
+        <h2>{props.user}</h2>
+        <p>
+          Front-end Developer passionate about creating modern web experiences.
+        </p>
+        <button>View Profile</button>
+      </div>
     </div>
   );
 };
