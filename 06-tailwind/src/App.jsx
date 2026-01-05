fa37908fe0c265a57df332cf8424162385c3c996
Toqeer Sheikh <102228843+toqeersheikh@users.noreply.github.com>
2026-01-05 17:00:01 +0500

    Using TailwindCSS

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <h1 className="bg-rose-500 text-8xl text-white p-4  text-center">
+        Hello React
+      </h1>
+    </div>
+  );
+};
+
+export default App;
